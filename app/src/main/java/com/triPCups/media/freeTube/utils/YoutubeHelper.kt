ec0395a848e668012c1d2661173b08cdda6a4f34
Eyal Yaakobi <unix14@gmail.com>
2025-09-06 04:40:01 +0300

    Fix URL parser and added deeplink urls to Manifest

--- app/src/main/java/com/triPCups/media/freeTube/utils/YoutubeHelper.kt
+++ app/src/main/java/com/triPCups/media/freeTube/utils/YoutubeHelper.kt
@@ -5,19 +5,35 @@ class YoutubeHelper {
     companion object {
 
         fun extractVideoIdFromUrl(url: String): String? {
-            val regex = "(?:https?:\\/\\/)?(?:www\\.)?(?:youtube\\.com\\/\\S*?(?:(?:\\/e(?:mbed))?\\/|watch\\/|v\\/|watch\\?v=|embed\\/|youtu.be\\/))([\\w\\-_]*)"
+            val regex = "(?:https?://)?(?:www\\.|m\\.)?(?:youtube\\.com/watch\\?v=|youtube\\.com/embed/|youtu\\.be/)([\\w-]{11})"
             val pattern = Regex(regex)
-
             val matchResult = pattern.find(url)
             return matchResult?.groupValues?.getOrNull(1)
         }
 
-        // Extracts timestamp from YouTube URL
         fun extractTimestampFromUrl(url: String): Int? {
-            val regex = "([?&]t=|[?&]start=)(\\d+)"
-            val pattern = Regex(regex)
-            val matchResult = pattern.find(url)
-            return matchResult?.groupValues?.getOrNull(2)?.toIntOrNull()
+            val regex = Regex("[?&](t|start)=([^&]+)")
+            val matchResult = regex.find(url)
+            val tValue = matchResult?.groups?.get(2)?.value ?: return null
+
+            var totalSeconds = 0
+            val timeRegex = Regex("(\\d+)([hms])")
+            val matches = timeRegex.findAll(tValue)
+
+            if (matches.any()) {
+                matches.forEach { m ->
+                    val amount = m.groupValues[1].toInt()
+                    when (m.groupValues[2]) {
+                        "h" -> totalSeconds += amount * 3600
+                        "m" -> totalSeconds += amount * 60
+                        "s" -> totalSeconds += amount
+                    }
+                }
+                return totalSeconds
+            } else {
+                // no time unit, parse as plain seconds
+                return tValue.toIntOrNull()
+            }
         }
     }
 }
